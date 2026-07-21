b6287ad5d26499ca878f13ea0d14ae86e7404991
misra-anupam <innocentangelfromhell@gmail.com>
2026-07-21 14:09:10 +0530

    Eyeballing bugfixes

--- LangGraph/subagents/orders/agent.py
+++ LangGraph/subagents/orders/agent.py
@@ -1,5 +1,20 @@
 from langgraph import StateGraph, END, START
+from .state import OrderState
 
+def route_by_intent(state: OrderState) -> str:
+    return state["action"]
+
+def delivery_address_check_router(state: OrderState) -> str:
+    if state["delivery_address_change_eligibility"]:
+        return "yes"
+    else:
+        return "no"
+
+def delivery_date_check_router(state: OrderState) -> str:
+    if state["delivery_date_change_eligibility"]:
+        return "yes"
+    else:
+        return "no"
 
 class Orders:
 
@@ -10,22 +25,22 @@ class Orders:
         """
         pass
 
-    def load_recent_orders(self, state, input):
+    def load_recent_orders(self, state):
 
         """
         Load recent top 5 orders.
-        Call MCP on /list
+        Call MCP on /list and save to state with key recent_orders = List[Order]
         """
         pass
 
-    def save_order_reference(self, state, input):
+    def save_order_reference(self, state):
 
         """
         Accept user selection and save order reference to state with key order=Order(key=...).
         """
         pass
 
-    def intent(self, state, input):
+    def intent(self, state):
 
         """
         Determine user action for the identified order
@@ -37,9 +52,11 @@ class Orders:
             - complaint regarding order items
             - talk to a human
             - redelivery
+
+        Save it to OrderActionType
         """
     
-    def track(self, state, input):
+    def track(self, state):
 
         """
         Track order status.
@@ -47,7 +64,7 @@ class Orders:
         """
         pass
 
-    def cancel(self, state, input):
+    def cancel(self, state):
 
         """
         Check if order can be cancelled.
@@ -57,56 +74,70 @@ class Orders:
         """
         pass
 
-    def change_delivery_date(self, state, input):
+    def check_if_delivery_date_can_be_changed(self, state):
 
         """
         Check if order has been delivered.
         If not delivered, can delivery date be changed?
         Call MCP on /date_change_eligibility with order
-        If yes, change delivery date.
-        If successful, /change_delivery_date with order and delivery date
+        Update state with key delivery_date_change_eligibility to True or False
         """
         pass
 
-    def change_delivery_address(self, state, input):
+    def change_delivery_date(self, state):
+
+        """
+        Accept from user the new delivery date
+        Call MCP on /change_delivery_date with order and new delivery date
+        """
+        pass
+
+    def check_if_delivery_address_can_be_changed(self, state):
 
         """
         Check if order has been delivered.
         Call MCP on /order_status
         If not delivered, can address be changed?
         Call MCP on /address_change_eligibility with order
-        If yes, change delivery address.        
-        If successful, /change_delivery_address with order and delivery address
+        Update state with key delivery_address_change_eligibility to True or False
         """
         pass
 
-    def load_order_items(self, state, input):
+    def change_delivery_address(self, state):
+
+        """
+        Accept from user the new delivery address
+        Call MCP on /change_delivery_address with order and new delivery address
+        """
+        pass
+
+    def load_order_items(self, state):
         
         """
         Show expected items in order.
-        Call MCP on /view_order with order
+        Call MCP on /view_order with order and save to state with key=List[Item]
         """
         pass
 
-    def identify_order_items_issue(self, state, input):
+    def identify_order_items_issue(self, state):
 
         """
         Understand what is the issue
         - defective
         - missing
         - wrong
-        Write to state with key order_item_issue=OrderItemIssue(issue=...)
+        Write to state with key order_item_issue=IssueType
         """
         pass
 
-    def save_issue_item(self, state, input):
+    def save_issue_item(self, state):
         
         """
-        Accept user selection and save issue item to state with key item=Item(key=...).
+        Accept user selection and save issue item to state with key issue_item=Item(key=...).
         """
         pass
 
-    def load_options_reorder_or_return_or_refund(self, state, input):
+    def load_options_reorder_or_return_or_refund(self, state):
 
         """
         Check policy if item can be refunded.
@@ -115,21 +146,21 @@ class Orders:
         """
         pass
 
-    def save_missing_defective_wrong_item_input(self, state, input):
+    def save_missing_defective_wrong_item_input(self, state):
         
         """
         Accept user input and accordingly trigger action. Save response to state with key return_mode
         """
         pass
 
-    def process_item_return(self, state, input):
+    def process_item_return(self, state):
 
         """
         Refund/reorder/return the order.
         """
         pass
 
-    def talk_to_human(self, state, input):
+    def talk_to_human(self, state):
         
         """
         HITL -> When not addressed by the above options
@@ -137,7 +168,7 @@ class Orders:
         """
         pass
 
-    def redelivery_request(self, state, input):
+    def redelivery_request(self, state):
         
         """
         Schedule delivery on today()+1 on /change_delivery_date
@@ -145,7 +176,7 @@ class Orders:
         """
         pass
 
-    def build_graph(self, state, input):
+    def build_graph(self):
 
         graph = StateGraph()
 
@@ -154,6 +185,8 @@ class Orders:
         graph.add_node("intent", self.intent)
         graph.add_node("track", self.track)
         graph.add_node("cancel", self.cancel)
+        graph.add_node("check_if_delivery_address_can_be_changed", self.check_if_delivery_address_can_be_changed)
+        graph.add_node("check_if_delivery_date_can_be_changed", self.check_if_delivery_date_can_be_changed)
         graph.add_node("change_delivery_date", self.change_delivery_date)
         graph.add_node("change_delivery_address", self.change_delivery_address)
         graph.add_node("load_order_items", self.load_order_items)
@@ -171,23 +204,32 @@ class Orders:
 
         graph.add_conditional_edges(
             "intent",
+            route_by_intent,
             {
-                state["action"] == "order-track": "track",
-                state["action"] == "order-cancel": "cancel",
-                state["action"] == "order-change delivery address": "change_delivery_address",
-                state["action"] == "order-change delivery date": "change_delivery_date",
-                state["action"] == "order-complaint regarding order items": "load_order_items",
-                state["action"] == "order-talk to human": "talk_to_human",
-                state["action"] == "order-redelivery request": "redelivery_request"
-            },
+                "order-track": "track",
+                "order-cancel": "cancel",
+                "order-change-delivery-address": "check_if_delivery_address_can_be_changed",
+                "order-change-delivery-date": "check_if_delivery_date_can_be_changed",
+                "order-complaint-regarding-order-items": "load_order_items",
+                "order-talk-to-human": "talk_to_human",
+                "order-redelivery-request": "redelivery_request"
+            }
+        )
+
+        graph.add_conditional_edge(
+            "check_if_delivery_address_can_be_changed",
+            delivery_address_check_router,
             {
-                "track": "track",
-                "cancel": "cancel",
-                "change_delivery_address": "change_delivery_address",
-                "change_delivery_date": "change_delivery_date",
-                "load_order_items": "load_order_items",
-                "talk_to_human": "talk_to_human",
-                "redelivery_request":"redelivery_request"
+                "yes":"change_delivery_address",
+                "no":END
+            }
+        )
+        graph.add_conditional_edge(
+            "check_if_delivery_date_can_be_changed",
+            delivery_date_check_router,
+            {
+                "yes":"change_delivery_date",
+                "no":END
             }
         )
 
--- LangGraph/subagents/orders/state.py
+++ LangGraph/subagents/orders/state.py
@@ -1,69 +1,38 @@
-from typing import TypedDict, List
+from typing import TypedDict, List, Literal, Annotated
 from pydantic import Field, BaseModel, field_validator
 from uuid import UUID
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
 
-allowed_actions = [
-    "order-track", "order-cancel", "order-change delivery address",
-    "order-change delivery date", "order-complaint regarding order items",
-    "order-talk to human", "order-redelivery request"
-    ]
-
-allowed_order_item_issues = [
+IssueType = Literal[
     "order-item-missing", "order-item-defective", "order-item-wrong"
 ]
 
-allowed_return_modes = [
+ReturnMode = Literal[
     "order-return-refund", "order-return-return", "order-return-reorder"
 ]
 
-class OrderAction(BaseModel):
-    action: str
-
-    @field_validator("action")
-    @classmethod
-    def validate_order_action(cls, value: str) -> str:
-
-        if value not in allowed_actions:
-            raise ValueError(f"action type must be one of {allowed_actions}")
-
-        return value
-
-class OrderItemIssue(BaseModel):
-    issue: str
-    
-    @field_validator("issue")
-    @classmethod
-    def validate_order_item_issue(cls, value: str) -> str:
-
-        if value not in allowed_order_item_issues:
-            raise ValueError(f"Issue type must be one of {allowed_order_item_issues}")
-
-        return value
-
-
-class ReturnOptions(BaseModel):
-    option: str
-    
-    @field_validator("option")
-    @classmethod
-    def validate_order_return_options(cls, value: str) -> str:
-
-        if value not in allowed_return_modes:
-            raise ValueError(f"Return mode must be one of {allowed_return_modes}")
-
-        return value
-
+OrderActionType = Literal[
+    "order-track", "order-cancel", "order-change-delivery-address",
+    "order-change-delivery-date", "order-complaint-regarding-order-items",
+    "order-talk-to-human", "order-redelivery-request"
+]
 class Order(TypedDict):
-    key: UUID4
+    key: UUID
 
 class Item(TypedDict):
-    key: UUID4
+    key: UUID
 
 class OrderState(TypedDict):
 
-    action: OrderAction
+    messages: Annotated[list[BaseMessage], add_messages]
+    action: OrderActionType
     order: Order
-    order_item_issue: OrderItemIssue
+    order_item_issue: IssueType
     issue_item: Item
-    return_options: List[ReturnOptions]
-    return_mode: str
+    return_options: List[ReturnMode]
+    return_mode: ReturnMode
+    recent_orders: List[Order]
+    order_items: List[Item]
+    delivery_address_change_eligibility: bool
+    delivery_date_change_eligibility: bool
